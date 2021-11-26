ec51b4f7b7e2971c5cbb895435a50f195e983872
Iker Morales <ikermorales@opendeusto.es>
2021-11-26 11:07:59 +0100

    GoogleCHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECK

--- stravaRegister/src/es/deusto/ingenieria/sd/auctions/currency/remote/IRegistrationGoogle.java
+++ stravaRegister/src/es/deusto/ingenieria/sd/auctions/currency/remote/IRegistrationGoogle.java
@@ -6,5 +6,5 @@ import java.rmi.RemoteException;
 
 public interface IRegistrationGoogle extends Remote {
 
-	public boolean checkUsuarioGoogle(String email, String contrasenya);
+	public boolean checkCuentaGmail(String email, String contrasenya);
 }
--- stravaRegister/src/es/deusto/ingenieria/sd/auctions/currency/remote/RegistrationGoogleService.java
+++ stravaRegister/src/es/deusto/ingenieria/sd/auctions/currency/remote/RegistrationGoogleService.java
@@ -1,6 +1,6 @@
 package es.deusto.ingenieria.sd.auctions.currency.remote;
 
-import java.io.BufferedReader;
+import java.io.BufferedReader; 
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,12 +12,10 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
-import es.deusto.ingenieria.sd.auctions.server.data.dto.EntrenamientoDTO;
-import es.deusto.ingenieria.sd.auctions.server.data.dto.TipoUsuarioDTO;
-import es.deusto.ingenieria.sd.auctions.server.data.dto.UserDTO;
-
 public class RegistrationGoogleService extends UnicastRemoteObject implements IRegistrationGoogle {
 	private static final long serialVersionUID = 1L;
+	
+	private HashMap<String, String> hashGoogle = new HashMap<>();
 
 	
 	//Attribute for the Singleton pattern
@@ -41,10 +39,20 @@ public class RegistrationGoogleService extends UnicastRemoteObject implements IR
 	}
 
 	@Override
-	public String getUsuarioGoogle(String nickName) {
-		HashMap<String, String> hashGoogle = new HashMap();
+	public boolean checkCuentaGmail(String email, String contrasenya) {
+		hashGoogle.put("billlie@gmail.com", "eleven");
+		hashGoogle.put("astro@gmail.com", "nauta");
+		hashGoogle.put("euwnoo@gmail.com", "1");
+		hashGoogle.put("rosalia@gmail.com", "rosalia");
+		hashGoogle.put("gorka@gmail.com", "gorka");
 		
+		if(hashGoogle.containsKey(email)) {
+			if(hashGoogle.get(email).matches(contrasenya)) {
+				return true;
+			}
+		}
 		
+		return false;
 	}
 
 	
